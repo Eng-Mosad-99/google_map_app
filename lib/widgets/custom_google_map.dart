ae536e3f902219db46bf1d83a8d668d064f27508
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2026-05-03 15:38:53 +0300

    create checkAndRequestLocationPermission method to check location permission

--- lib/widgets/custom_google_map.dart
+++ lib/widgets/custom_google_map.dart
@@ -221,6 +221,17 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
         //! TODO: show error bar
       }
     }
+    checkAndRequestLocationPermission();
+  }
+
+  void checkAndRequestLocationPermission() async {
+    var permissionStatus = await location.hasPermission();
+    if (permissionStatus == PermissionStatus.denied) {
+      permissionStatus = await location.requestPermission();
+      if (permissionStatus != PermissionStatus.granted) {
+        //! TODO: show error bar
+      }
+    }
   }
 }
 
