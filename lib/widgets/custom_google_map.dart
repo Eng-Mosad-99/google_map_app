d119826062fa481bf4b44df13741bb7b7a3eb2d0
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2026-05-04 16:27:41 +0300

    refactor code

--- lib/widgets/custom_google_map.dart
+++ lib/widgets/custom_google_map.dart
@@ -1,8 +1,6 @@
-import 'dart:convert';
-import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:google_maps_app/models/place_model.dart';
+import 'package:google_maps_app/utils/location_service.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 
@@ -17,19 +15,17 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
   late CameraPosition initialCameraPosition;
   GoogleMapController? googleMapController;
   late Location location;
+  late LocationService locationService;
   @override
   void initState() {
     super.initState();
     initMapStyle();
-    // initMarkers();
-    // initPolyline();
-    // initPolygon();
-    // initCircle();
     initialCameraPosition = CameraPosition(
       target: LatLng(30.762578494071878, 31.31647130125389),
       zoom: 12,
     );
     location = Location();
+    locationService = LocationService();
     updateMyLocation();
   }
 
@@ -43,140 +39,6 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
       _mapStyle = style;
     });
   }
-
-  // Set<Marker> markers = {};
-
-  // Future<Uint8List> getImageFromRawData(String image, double width) async {
-  //   var imageData = await rootBundle.load(image);
-  //   var imageCodec = await ui.instantiateImageCodec(
-  //     imageData.buffer.asUint8List(),
-  //     targetWidth: width.round(),
-  //   );
-  //   var imageFrame = await imageCodec.getNextFrame();
-  //   var imageByteData = await imageFrame.image.toByteData(
-  //     format: ui.ImageByteFormat.png,
-  //   );
-  //   return imageByteData!.buffer.asUint8List();
-  // }
-
-  // Future<BitmapDescriptor> getColoredMarker(Color color) async {
-  //   final ByteData data = await rootBundle.load(
-  //     'assets/images/marker_image.png',
-  //   );
-  //   final Uint8List bytes = data.buffer.asUint8List();
-
-  //   final ui.Codec codec = await ui.instantiateImageCodec(bytes);
-  //   final ui.FrameInfo frame = await codec.getNextFrame();
-  //   final ui.Image image = frame.image;
-
-  //   final recorder = ui.PictureRecorder();
-  //   final canvas = Canvas(recorder);
-
-  //   final paint = Paint()
-  //     ..colorFilter = ColorFilter.mode(color, BlendMode.srcIn);
-
-  //   canvas.drawImage(image, Offset.zero, paint);
-
-  //   final picture = recorder.endRecording();
-  //   final img = await picture.toImage(image.width, image.height);
-  //   final byteData = await img.toByteData(format: ui.ImageByteFormat.png);
-
-  //   return BitmapDescriptor.bytes(byteData!.buffer.asUint8List(), width: 30);
-  // }
-
-  // void initMarkers() async {
-  //   // var markerAssetImage = BitmapDescriptor.bytes(
-  //   //   await getImageFromRawData('assets/images/marker_image.png', 30),
-  //   // );
-  //   var customIcon = await getColoredMarker(Colors.red);
-  //   //  BitmapDescriptor.asset(
-  //   //   width: 30,
-  //   //   ImageConfiguration.empty,
-  //   //   'assets/images/marker_image.png',
-  //   // );
-  //   var myMarkers = places
-  //       .map(
-  //         (place) => Marker(
-  //           icon: customIcon,
-  //           infoWindow: InfoWindow(title: place.name, snippet: place.name),
-  //           position: place.latLng,
-  //           markerId: MarkerId(place.id.toString()),
-  //         ),
-  //       )
-  //       .toSet();
-  //   markers.addAll(myMarkers);
-  //   setState(() {});
-  // }
-
-  // Set<Polyline> polylines = {};
-  // void initPolyline() {
-  //   Polyline polyline = Polyline(
-  //     color: Colors.red,
-  //     geodesic: true,
-  //     width: 5,
-  //     zIndex: 1,
-  //     // patterns: [PatternItem.dot],
-  //     startCap: Cap.roundCap,
-  //     polylineId: PolylineId('1'),
-  //     points: [
-  //       LatLng(53.61442606514419, 28.824231157958256),
-  //       LatLng(-22.292064936189306, 23.603794379853255),
-  //       // LatLng(30.76473611852077, 31.319744984043357),
-  //       // LatLng(30.764026251422194, 31.32430473923952),
-  //       // LatLng(30.767667721980498, 31.320667663959618),
-  //     ],
-  //   );
-  //   // Polyline polyline2 = Polyline(
-  //   //   color: Colors.green,
-  //   //   width: 5,
-  //   //   zIndex: 3,
-  //   //   startCap: Cap.roundCap,
-  //   //   polylineId: PolylineId('2'),
-  //   //   points: [
-  //   //     LatLng(30.76731740928899, 31.324723163905162),
-  //   //     LatLng(30.766192712544985, 31.319380203757),
-  //   //     LatLng(30.762505090012066, 31.322126785704118),
-  //   //     LatLng(30.76158316231281, 31.31896177919064),
-  //   //   ],
-  //   // );
-  //   polylines.add(polyline);
-  //   // polylines.add(polyline2);
-  // }
-
-  // Set<Polygon> polygons = {};
-  // void initPolygon() {
-  //   Polygon polygon = Polygon(
-  //     holes: [
-  //       [
-  //         LatLng(30.764980080722573, 31.321875755083035),
-  //         LatLng(30.766244722433967, 31.32223990384524),
-  //         LatLng(30.766645413989025, 31.322002958030883),
-  //       ],
-  //     ],
-  //     fillColor: Colors.red.withValues(alpha: .4),
-  //     strokeColor: Colors.red.withValues(alpha: .4),
-  //     strokeWidth: 2,
-  //     polygonId: PolygonId('1'),
-  //     points: [
-  //       LatLng(30.76731740928899, 31.324723163905162),
-  //       LatLng(30.766192712544985, 31.319380203757),
-  //       LatLng(30.762505090012066, 31.322126785704118),
-  //       LatLng(30.76158316231281, 31.31896177919064),
-  //     ],
-  //   );
-  //   polygons.add(polygon);
-  // }
-
-  // Set<Circle> circles = {};
-  // void initCircle() {
-  //   Circle circle = Circle(
-  //     circleId: CircleId('1'),
-  //     center: LatLng(30.76496683364935, 31.321115345435743),
-  //     radius: 5000,
-  //     fillColor: Colors.red.withValues(alpha: .4),
-  //   );
-  //   circles.add(circle);
-  // }
 
   Set<Marker> markers = {};
   @override
@@ -193,81 +55,62 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
           },
           // markers: markers,
         ),
-        // Positioned(
-        //   left: 16,
-        //   right: 16,
-        //   bottom: 16,
-        //   child: ElevatedButton(
-        //     onPressed: () {
-        //       googleMapController.animateCamera(
-        //         CameraUpdate.newCameraPosition(
-        //           CameraPosition(
-        //             zoom: 18,
-        //             target: LatLng(30.768090681310408, 31.32845033933248),
-        //           ),
-        //         ),
-        //       );
-        //     },
-        //     child: Text('Change Location'),
-        //   ),
-        // ),
       ],
     );
   }
 
-  Future<void> checkAndRequestLocationService() async {
-    var isServiceEnabled = await location.serviceEnabled();
-    if (!isServiceEnabled) {
-      isServiceEnabled = await location.requestService();
-      if (!isServiceEnabled) {
-        //! TODO: show error bar
-      }
-    }
-  }
+  // void getLocationData() {
+  //   location.changeSettings(distanceFilter: 2);
 
-  Future<bool> checkAndRequestLocationPermission() async {
-    var permissionStatus = await location.hasPermission();
-    if (permissionStatus == PermissionStatus.deniedForever) {
-      return false;
-    }
-    if (permissionStatus == PermissionStatus.denied) {
-      permissionStatus = await location.requestPermission();
-      if (permissionStatus != PermissionStatus.granted) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  void getLocationData() {
-    location.changeSettings(
-      distanceFilter: 2,
-    );
-    location.onLocationChanged.listen((locationData) {
-      var cameraPosition = CameraPosition(
-        zoom: 15,
-        target: LatLng(locationData.latitude!, locationData.longitude!),
-      );
-      var myLocationMarker = Marker(
-        markerId: MarkerId('my_location_marker'),
-        position: LatLng(locationData.latitude!, locationData.longitude!),
-      );
-      markers.add(myLocationMarker);
-      setState(() {
-
-      });
-      googleMapController?.animateCamera(
-        CameraUpdate.newCameraPosition(cameraPosition),
-      );
-    });
-  }
+  //   location.onLocationChanged.listen((locationData) {
+  //     var cameraPosition = CameraPosition(
+  //       zoom: 15,
+  //       target: LatLng(locationData.latitude!, locationData.longitude!),
+  //     );
+  //     var myLocationMarker = Marker(
+  //       markerId: MarkerId('my_location_marker'),
+  //       position: LatLng(locationData.latitude!, locationData.longitude!),
+  //     );
+  //     markers.add(myLocationMarker);
+  //     setState(() {});
+  //     googleMapController?.animateCamera(
+  //       CameraUpdate.newCameraPosition(cameraPosition),
+  //     );
+  //   });
+  // }
 
   void updateMyLocation() async {
-    await checkAndRequestLocationService();
-    bool hasPermission = await checkAndRequestLocationPermission();
+    await locationService.checkAndRequestLocationService();
+    bool hasPermission = await locationService
+        .checkAndRequestLocationPermission();
     if (hasPermission) {
-      getLocationData();
+      location.changeSettings(distanceFilter: 2);
+      locationService.getRealTimeLocationData((locationData) {
+        location.onLocationChanged.listen((locationData) {
+          setMyLocationMarker(locationData);
+          setMyCameraPosition(locationData);
+        });
+      });
     } else {}
+  }
+
+  void setMyCameraPosition(LocationData locationData) {
+    var cameraPosition = CameraPosition(
+      zoom: 15,
+      target: LatLng(locationData.latitude!, locationData.longitude!),
+    );
+    googleMapController?.animateCamera(
+      CameraUpdate.newCameraPosition(cameraPosition),
+    );
+  }
+
+  void setMyLocationMarker(LocationData locationData) {
+    var myLocationMarker = Marker(
+      markerId: MarkerId('my_location_marker'),
+      position: LatLng(locationData.latitude!, locationData.longitude!),
+    );
+    markers.add(myLocationMarker);
+    setState(() {});
   }
 }
 
